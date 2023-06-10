71579fb0e0fdf5f2ce6ff27197b63358d45b00f0
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-06-10 11:48:06 +0300

    Completed looping code lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(inputArray, eventName) {
+    const arr1 = [];
+    for(let i = 0; i < inputArray.length; i++) {
+        arr1.push(`Thank you, ${inputArray[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return arr1
+}
+
+function countDown(n) {
+    while(n >= 0) {
+        console.log(n--)
+    }
+}
